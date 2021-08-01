30eebfd206b7385f78240cfe508e93c4260cc9cc
hujinbin <437044048@qq.com>
2021-08-01 18:57:36 +0800

    test: 单元测试

--- test/index.js
+++ test/index.js
@@ -5,4 +5,10 @@ describe('pullRefresh.vue', () => {
     it('pullRefresh是否存在',()=>{
         expect(pullRefresh).to.be.ok;
     })
+    it('测试是否生效', () => {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor().$mount()
+        expect(vm.$el.querySelector('.loading').textContent)
+          .to.equal('加载中...')
+    })
 })
