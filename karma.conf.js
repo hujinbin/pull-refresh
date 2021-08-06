fac7d5574e3c9db997866bc2d3bdf03effd73eb6
hujinbin <437044048@qq.com>
2021-08-06 18:14:03 +0800

    test: 自动化测试调试

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine','sinon-chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'dist/*.test.js',
+      'dist/*.test.css',
     ],
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,16 @@
+const expect = chai.expect;
 import Vue from 'vue'
 import pullRefresh from '../src/index'
+
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
 
 describe('pullRefresh.vue', () => {
     it('pullRefresh是否存在',()=>{
         expect(pullRefresh).to.be.ok;
     })
     it('测试是否生效', () => {
-        const Constructor = Vue.extend(Button)
+        const Constructor = Vue.extend(pullRefresh)
         const vm = new Constructor().$mount()
         expect(vm.$el.querySelector('.loading').textContent)
           .to.equal('加载中...')
